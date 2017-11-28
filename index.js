ebaf0a311cf38631ef7b830bc37cfba88343b9bf
Ryan Inawat <rrinawat@gmail.com>
2017-11-28 02:44:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function destructivelyRemoveLastKitten(name){
   return kittens.pop()
 }
 
-funtion destructivelyRemoveFirstKitten(name){
+function destructivelyRemoveFirstKitten(name){
   return kittens.shift()
 }
